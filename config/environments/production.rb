c6f5fbd33d4fdef6c38a3ec742d35f772be7118f
Lisa Friedman <lisacf@gmail.com>
2013-09-10 08:34:38 -0700

    config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Cupcake::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   #config.assets.precompile += ['google_maps.css']
 
   # Generate digests for assets URLs.
