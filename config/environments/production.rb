890f692b252f95e5cf576a489b35efb72569f6ff
Lisa Friedman <lisacf@gmail.com>
2013-09-09 17:00:41 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Cupcake::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile += ['google_maps.css']
+  #config.assets.precompile += ['google_maps.css']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
