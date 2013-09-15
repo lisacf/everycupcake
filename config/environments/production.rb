4748f74576d23108ee2293d90c9415278a582718
Lisa Friedman <lisacf@gmail.com>
2013-09-14 21:13:53 -0700

    update assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Cupcake::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  #config.assets.precompile += ['google_maps.css']
+  #config.assets.precompile += ['google_maps.css', 'mastercakes.css.scss', 'stores.css.scss', 'foundation_and_overrides.scss', 'mastercakes.css.scss' ]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
